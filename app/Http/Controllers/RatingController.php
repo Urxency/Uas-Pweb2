4a5c0bc785c27d2f2459c94bbfb264f826c50e15
Luvas <alief.fattarohman@gmail.com>
2025-07-06 18:11:50 +0700

    nambahain seeder

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Rating;
 use Illuminate\Http\Request;
+use App\Models\Resep;
+use Illuminate\Support\Facades\Auth;
 
 class RatingController extends Controller
 {
-    //
+    public function store(Request $request, Resep $recipe)
+    {
+        $validated = $request->validate([
+            'nilai' => 'required|integer|min:1|max:5',
+        ]);
+
+        $rating = Rating::updateOrCreate(
+            [
+                'user_id' => Auth::id(),
+                'recipe_id' => $recipe->id,
+            ],
+            [
+                'nilai' => $validated['nilai']
+            ]
+        );
+
+        return redirect()->back()
+            ->with('success', 'Rating berhasil disimpan!');
+    }
 }
--- database/seeders/ResepSeeder.php
+++ database/seeders/ResepSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Kategori;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,18 @@ class ResepSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $categories = [
+            ['nama_kategori' => 'Nasi'],
+            ['nama_kategori' => 'Mie'],
+            ['nama_kategori' => 'Gorengan'],
+            ['nama_kategori' => 'Sayur'],
+            ['nama_kategori' => 'Lauk'],
+            ['nama_kategori' => 'Minuman'],
+            ['nama_kategori' => 'Cemilan'],
+        ];
+
+        foreach ($categories as $category) {
+            Kategori::create($category);
+        }
     }
 }
